9ebb697ebd8ee0f2fdd41088d34a6791a07a9b50
wzZero <2464983868@qq.com>
2020-07-12 10:48:51 +0800

    move image

--- ImageEditor/Common/image.cpp
+++ ImageEditor/Common/image.cpp
@@ -12,21 +12,15 @@ void Image::Sharp()
                 -1,10, -1,
                 0, -1, 0);
     filter2D(this->currentImage,this->currentImage, CV_8UC1, sharpkernel);
-    //this->ImgShow();
 }
 
 void Image::Gray()
 {
-    //PushStack();
-    //isGray=true;
     cvtColor(this->currentImage,this->currentImage,COLOR_BGR2GRAY);
-    //this->ImgShow();
 }
 
 void Image::Blur()
 {
-    //PushStack();
-    //isBlur-true;
+
     blur(this->currentImage, this->currentImage, Size(5,5));
-    //this->ImgShow();
 }
